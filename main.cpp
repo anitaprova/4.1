661d487780fbd239449f81cce97c8e0a1eefb85c
anitaprova <ANITA.PROVA49@myhunter.cuny.edu>
2022-09-15 12:41:11 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Author: Anita Prova
+ * Course: CSCI-135
+ * Instructor: Genady Maryash
+ * Assignment: Homework E4.8
+ * The program prints the characters of a string on separate lines. 
+ * */
+
 #include <iostream>
 #include <string>
 
